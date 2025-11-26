f33dea18311eac6907db3336e9ebc3ddeeda3802
Pablo Carvalho <pablo.luan.carvalho@gmail.com>
2025-11-26 23:52:04 +0000

    feat: adding movie group

--- movieflix/src/main/java/com/movieflix/controller/MovieController.java
+++ movieflix/src/main/java/com/movieflix/controller/MovieController.java
@@ -32,4 +32,11 @@ public class MovieController {
                 .toList());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieResponse> findById(@PathVariable Long id) {
+        return movieService.findById(id)
+                .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
