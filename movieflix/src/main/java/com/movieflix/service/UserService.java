edd292bd15d0a921c0c785f62bb9354a6b1b6679
Pablo Carvalho <pablo.luan.carvalho@gmail.com>
2025-11-30 01:26:39 +0000

    Adding user

---
+++ movieflix/src/main/java/com/movieflix/service/UserService.java
@@ -0,0 +1,13 @@
+package com.movieflix.service;
+
+import com.movieflix.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+}
